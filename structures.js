76640bb22fd5a1c104ef954e7f49bbe2fc5319b7
adnan007d <adnanmansuri54@gmail.com>
2021-06-02 19:43:27 +0530

    Added like and comment structure

--- structures.js
+++ structures.js
@@ -5,6 +5,9 @@ const user = {
   image: String, // ? do we need this
 };
 
+// All the post will be stored under a posts collection something like this
+// posts/{post1}
+// posts/{post2}
 const post = {
   likeCount: Number,
   commentCount: Number,
@@ -24,4 +27,17 @@ const loginProviders = {
   google,
   facebook,
   apple,
+};
+
+// All the likes will be stored under a single likes collection just like post
+const like = {
+  user: user, // The user who liked the post
+  postId: String, // The id of the liked post
+};
+
+// Comments will also be stored under a collection called comments
+const comment = {
+  user: user,
+  postId: String,
+  comment: String,
 };
